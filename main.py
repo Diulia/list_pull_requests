af88b8576d811b28b30e1e6e76e136d409bb5409
Diulia C. Godoy Maciel <diuliac.pf@gmail.com>
2022-11-03 19:16:20 -0300

    edit main

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ parser=argparse.ArgumentParser()
 
 parser.add_argument("--page", help="Pagination Index")
 
+
 args=parser.parse_args()
 
-response = requests.get('https://api.github.com/repos/Diulia/webscraping-python/pulls?state=open')
+response = requests.get('https://api.github.com/repos/Diulia/webscraping-python/pulls?state=all')
 
 # print('Page content:',json.dumps(response.json(), indent=2))
 for pr in response.json(): 
